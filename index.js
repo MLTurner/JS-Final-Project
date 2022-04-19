829eb63e9de83d3372c5468ba21d23fbeb63c240
MLTurner <mlturne3@ncsu.edu>
2022-04-18 21:31:02 -0400

    changed game, got new assets and html

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-
-
-
-let linkCharacter = 
+//defining our icons of rock, paper, scissors
+const rock = ('assets/rock.png')
+const paper = ('assets/paper.png')
+const scissors = ('assets/scissors.png')
 
 
 //defining our 
