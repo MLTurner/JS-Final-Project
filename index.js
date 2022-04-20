18f798af35488188d9e377a806483f14680d019e
MLTurner <mlturne3@ncsu.edu>
2022-04-20 18:51:48 -0400

    switched to font awesome icons; made functions and event listeners

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 //defining our icons of rock, paper, scissors
-const rock = ('assets/rock.png')
-const paper = ('assets/paper.png')
-const scissors = ('assets/scissors.png')
+const choices = document.querySelectorAll('.fa-solid');
 
+//defining our score
+const score = document.getElementById('score');
+const result = document.getElementById('result');
+const restart = document.getElementById('restart');
+const modal = document.querySelector('.modal');
+const scoreboard = {
+    player: 0,
+    computer: 0
+}
 
-//defining our 
+// Play game
+function play(e) {
+    restart.style.display = 'inline-block';
+    const playerChoice = e.target.id;
+}
+
+// Event Listeners to loop through our choices
+choices.forEach(choice => choice.addEventListener('click', play));
